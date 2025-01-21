34ab2fb96a932755b94679c57d943008767d5668
sai <sai@in28min.com>
2025-01-21 17:07:05 -0600

    ExceptionHandling

---
+++ src/test/java/exceptionHandling/customExceptionHandling/CustomExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package exceptionHandling.customExceptionHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
